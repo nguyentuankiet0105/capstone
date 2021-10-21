f420075daf003a1c292c28431ec5fd829d89c93e
Hàn Kiêu <nguyentuankiet0105@gmail.com>
2021-10-21 22:14:02 +0700

    reupdate client

---
+++ src/Component/Layout/WorkSpace/WorkSpace_Sub/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <h1>Home</h1>
+        <p>ndjsdjn</p>
+        <div>Slide Show</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
